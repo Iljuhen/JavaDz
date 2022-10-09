78c9207d6cf579cd67364bb9cf3cb40418c4708c
ilja <bucas@yandex.ru>
2022-10-09 19:32:26 +0300

    ДЗ 3 и ДЗ4

---
+++ dz5/dz5.java
@@ -0,0 +1,81 @@
+package DZ.dz5;
+
+import java.util.HashMap;
+import java.util.Map;
+
+;
+
+public class dz5 {
+    public static void main(String[] args) {
+
+        // ===========Задача 1
+        // Объявить и инициализировать словарь hashMap<String, String>
+
+        System.out.println(ANSI_GREEN + "Задача 1" + ANSI_RESET);
+        HashMap<String, String> hashmap = new HashMap<>();
+        System.out.println("HashMap<String, String> hashmap = new HashMap<>();");
+        
+        // ===========Задача 2
+        // Добавить в словарь 10 пар "Ключ - Значение". 
+        // Автор и название книги например.
+        System.out.println(ANSI_GREEN + "Задача 2" + ANSI_RESET);
+        hashmap.put("sun", "Ilia");
+        hashmap.put("daughter", "Yasya");
+        hashmap.put("wife", "Kate");
+        hashmap.put("cat", "Alice cat");
+        hashmap.put("dog", "Vichy");
+        hashmap.put("mother-in-law", "Masha");
+        hashmap.put("father-in-law", "Kolja");
+        hashmap.put("mum", "Olja");
+        hashmap.put("dad", "Sergey");
+        hashmap.put("sister", "Lida");
+        hashmap.forEach((key, value) -> System.out.println(key + ", " + value + " ; "));
+
+        // ===========Задача 3
+        // Пройти по словарю и добавить к значениям символ "!"
+        System.out.println(ANSI_GREEN + "Задача 3" + ANSI_RESET);
+
+        for (Map.Entry entry : hashmap.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue() + "!");
+        }
+
+        // ===========Задача 4
+        // Добавить нового автора и название книги если соответствующего ключа не было.
+        System.out.println(ANSI_GREEN + "Задача 4" + ANSI_RESET);
+
+        hashmap.putIfAbsent("bird", "Вася" + "----------> добавили, так как ключа не было");
+        hashmap.forEach((key, value) -> System.out.println(key + ", " + value + " ; "));
+        hashmap.remove("bird");
+
+        // ===========Задача 5
+        // Если ключ, указанный в запросе, имеется, вырезать из соответствующего
+        // ключу значения первое слово.
+        System.out.println(ANSI_GREEN + "Задача 5" + ANSI_RESET);
+
+        if (hashmap.containsKey("cat") == true) {
+            hashmap.replace("cat", "cat");
+            hashmap.forEach((key, value) -> System.out.println(key + " ; " + value));
+        }
+        System.out.println();
+        // ===========Задача 6
+        // Если ключ, указанный в запросе, имеется, вырезать из соответствующего
+        // ключу значения первое слово.
+        System.out.println(ANSI_GREEN + "Задача 6" + ANSI_RESET);
+        hashmap.forEach((key, value) -> System.out.println(key + "  --->  " + value + " ; "));
+
+        // ===========Задача 7
+        // * Пройти по словарю другим, не таким как в пункте 3 и 6, методом.
+        // Добавить к значениям строку "(просмотрен)" и вывести в консоль.
+
+        System.out.println(ANSI_GREEN + "Задача 7" + ANSI_RESET);
+        for (String str : hashmap.keySet()) {
+            hashmap.put(str, hashmap.get(str) + "  (просмотрен)\n");
+        }
+        System.out.print(hashmap);
+
+    }
+
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_RESET = "\u001B[37m";
+
+}
---
+++ dz6/dz6.java
@@ -0,0 +1,33 @@
+package JavaDz.dz6;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Random;
+import java.util.TreeSet;
+
+public class dz6 {
+
+    public static void main(String[] args) {
+
+        HashSet<Integer> hs = new HashSet<>();
+        LinkedHashSet<Integer> lhs = new LinkedHashSet<>();
+        TreeSet<Integer> ts = new TreeSet<>();
+
+        for (int index = 0; index < 10; index++) {
+
+            int r = new Random().nextInt(10);
+            System.out.print(r + " ;");
+            hs.add(r);
+            lhs.add(r);
+            ts.add(r);
+
+        }
+        System.out.println();
+        hs.forEach(n -> System.out.print(n+ " ;"));
+        System.out.println();
+        lhs.forEach(n -> System.out.print(n+ " ;"));
+        System.out.println();
+        ts.forEach(n -> System.out.print(n+ " ;"));
+        System.out.println();
+    }
+}
