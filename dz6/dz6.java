2a1e86c505d6bb135aaa9c18b2babf47d040a468
ilja <bucas@yandex.ru>
2022-10-12 22:50:17 +0300

    ДЗ 6 задача 5 и 6

--- dz6/dz6.java
+++ dz6/dz6.java
@@ -87,9 +87,40 @@ public class dz6 {
         // Объявить и инициализировать множество TreeSet ts1 с компаратором Integer (-1 при значении %2==0 ;0 при ==;1 при значении %2!=0).
         // Заполнить ts1 15 случайными числами и вывести на печать.
 
+        Comparator<Integer> comparatorSet = new Comparator<Integer>() {
+            public int compare(Integer t0, Integer t1) {
+                return ((t0 - t1) % 2 == 0) ? -1 : ((t0 - t1 % 2) != 0) ? 1 : 0;
+            }
+        };
+
+        TreeSet<Integer> ts1 = new TreeSet<>(comparatorSet);
+        for (int i = 0; i < 15; i++) {
+            int numb = new Random().nextInt(30);
+            ts1.add(numb);
+        }
+        System.out.println("TreeSet ts1 с компаратором");
+        ts1.forEach(System.out::println);
+        System.out.println();
+
         // ===========Задача 6
         // *Объявить и инициализировать множество TreeMap tm с компаратором Integer (-1 при значении %2==0 ;0 при ==;1 при значении %2!=0).
         // Заполнить tm 15 случайными числами и вывести на печать.
+
+        Comparator<Integer> comparatorMap = new Comparator<Integer>() {
+            public int compare(Integer t0, Integer t1) {
+                return ((t0 - t1)%2==0)?-1:((t0 - t1)%2!=0)?1:0;
+            }
+        };
+
+        TreeMap<Integer, Integer> tm = new TreeMap<>(comparatorMap);
+        for (int i = 0; i < 15; i++) {
+            int numb1 = new Random().nextInt(30);
+            int numb2 = new Random().nextInt(30);
+
+            tm.put(numb1, numb2);
+        }
+        System.out.println("TreeMap tm с компаратором");
+        System.out.println(tm);
 
     }
 
